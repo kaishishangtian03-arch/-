37eb92594b7a02dba9855b3b22b7a3dd17f3d74a
kaishishangtian03-arch <kaishishangtian03@gmail.com>
2026-08-27 18:16:35 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = {
+    "updated_at": "準備中...",
+    "routes": [
+        {
+            "route_name": "34号系統（大阪駅前 ➔ 守口車庫前）",
+            "buses": []
+        },
+        {
+            "route_name": "36号系統（大阪駅前 ➔ 地下鉄門真南）",
+            "buses": []
+        }
+    ]
+};
